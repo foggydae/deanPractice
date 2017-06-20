030daa565881d1d585d6eaec092a2dbf2bccc986
smallst <tongsmallst@gmail.com>
2017-06-20 10:51:07 +0800

    session | simple login
    debug url redirection
    
    debug url redirection
    
    session debug

--- controllers/login.js
+++ controllers/login.js
@@ -1,9 +1,25 @@
+const User = global.model.User;
 var loginController = async (ctx, next) => {
     console.log(ctx.request.body);
 	var id = ctx.request.body.id || '',
 		passwd = ctx.request.body.passwd || '';
-		ctx.body = {code:200,body:"hello, login"};
-}
+    await User.findOne({
+        where:{
+            userId: id,
+            passwd: passwd
+        },
+        raw: true
+    }).then(res => {
+        console.log(res);
+        if(res.userId === id)
+        {
+            ctx.session.id = id;
+            ctx.session.userState = res.rol;
+		    ctx.body = {code:200,content:res.rol};
+        }
+    })
+
+};
 module.exports = {
 	"POST /login" : loginController
 }
